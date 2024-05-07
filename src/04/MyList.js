63de1278614be0fcdd0bf5efa2b31708778ccc47
hahahahohohoho <tjstls9728@gmail.com>
2024-05-07 17:44:10 +0900

    리액트 페이지 만들기(5. 7.)

--- src/04/MyList.js
+++ src/04/MyList.js
@@ -3,7 +3,8 @@ import MyListItems from "./MyListItems";
 
 export default function MyList() {
   const tags = MyListData.map(item =>
-                                <MyListItems title = {item.title}
+                                <MyListItems key = {item.title}  // 동적으로 만들 때 반드시 키값을 줘야한다. 안주면 콘솔창에 에러남!
+                                            title = {item.title}
                                             imgUrl = {item.imgUrl}
                                             content = {item.content}/>)
   return (
--- src/04/MyListItems.js
+++ src/04/MyListItems.js
@@ -7,19 +7,19 @@ export default function MyListItems({title, imgUrl, content}) {
   }
 
   return (
-    <div className="flex w-full justify-center bg-slate-100">
+    <div className="flex w-full justify-center bg-slate-300">
       <div className="flex justify-center items-center w-1/3">
         <img src={imgUrl} alt = {imgUrl}/>
       </div>
       <div className="flex flex-col w-2/3 p-2">
-        <div className="text-3xl bold h-1/5">{title}</div>
+        <div className="text-3xl bold h-1/5 font-bold">{title}</div>
         <div className="py-1.5 text-l h-3/5">
             {content}
         </div>
         <div className="py-1.5 mb-0 h-1/5 flex justify-end">
-            <span className="px-0.5" onClick={handleCount}>❤</span>
-            <span className="px-0.5">Like</span>
-            <span className="px-0.5">{n}</span>
+            <span onClick={handleCount}>❤</span>
+            <span className="px-2 font-bold" onClick={handleCount}>Like</span>
+            <span>{n}</span>
         </div>
       </div>
     </div>
